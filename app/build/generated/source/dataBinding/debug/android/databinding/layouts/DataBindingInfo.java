be9dfcc8bf9728e4c934bfff4e4889a683bd461a
Vipin Vasu <vipin.vasu@sigmainfo.com>
2018-02-27 10:57:43 +0530

    Code clean up

--- app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
+++ app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -2,5 +2,5 @@ package android.databinding.layouts;
 
 import android.databinding.BindingBuildInfo;
 
-@BindingBuildInfo(buildId="72b74f76-a5a9-438c-9a95-17b8f63f2073")
+@BindingBuildInfo(buildId="325629df-d4e6-42e7-aa90-192f1ef92b4e")
 public class DataBindingInfo {}
